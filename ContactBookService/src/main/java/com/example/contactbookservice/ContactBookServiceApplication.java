5c22d8bf13c61b16afc45fbb1997839b52e86c8d
Leandro Fonseca <leandro_dinis@hotmail.com>
2024-01-16 01:37:13 +0000

    Initial commit

---
+++ ContactBookService/src/main/java/com/example/contactbookservice/ContactBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.contactbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactBookServiceApplication.class, args);
+    }
+
+}
